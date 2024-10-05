19a74ee95af2535ce143ea088bd7cd1a0ad9631c
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-10-05 11:00:35 +0300

    detail screen added

---
+++ app/src/main/java/com/atilsamancioglu/landmarkbookcompose/DetailsScreen.kt
@@ -0,0 +1,67 @@
+package com.atilsamancioglu.landmarkbookcompose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun DetailScreen(
+    landmark: Landmark,
+    navController: NavController
+) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Black),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Text(text = landmark.name,
+                style = MaterialTheme.typography.displayLarge,
+                modifier = Modifier.padding(2.dp),
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                textAlign = TextAlign.Center
+            )
+
+            Image(bitmap = ImageBitmap.imageResource(id = landmark.image),
+                contentDescription = landmark.name,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .size(200.dp, 200.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.Gray, CircleShape)
+            )
+
+            Text(text = landmark.country,
+                style = MaterialTheme.typography.displaySmall,
+                modifier = Modifier.padding(2.dp),
+                fontWeight = FontWeight.Bold,
+                color = Color.Green,
+                textAlign = TextAlign.Center
+
+            )
+
+        }
+    }
+}
--- app/src/main/java/com/atilsamancioglu/landmarkbookcompose/MainActivity.kt
+++ app/src/main/java/com/atilsamancioglu/landmarkbookcompose/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.atilsamancioglu.landmarkbookcompose.ui.theme.LandmarkBookComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +30,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             LandmarkBookComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(modifier = Modifier.padding(innerPadding))
+                    Box(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        MainScreen()
+
+                    }
                 }
             }
         }
@@ -28,7 +43,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen() {
     val landmarkList = ArrayList<Landmark>()
     val pisa = Landmark("Pisa","Italy",R.drawable.pisa)
     val colosseum = Landmark("Colloseum","Italy",R.drawable.colosseum)
@@ -38,7 +53,47 @@ fun MainScreen(modifier: Modifier = Modifier) {
     landmarkList.add(colosseum)
     landmarkList.add(eiffel)
     landmarkList.add(londonBridge)
+    landmarkList.add(pisa)
+    landmarkList.add(colosseum)
+    landmarkList.add(eiffel)
+    landmarkList.add(londonBridge)
+    landmarkList.add(pisa)
+    landmarkList.add(colosseum)
+    landmarkList.add(eiffel)
+    landmarkList.add(londonBridge)
 
+    CryptoList(landmarks = landmarkList)
+
+}
+
+
+@Composable
+fun CryptoList(landmarks: List<Landmark>) {
+    LazyColumn(contentPadding = PaddingValues(5.dp),
+    modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.primaryContainer))
+     {
+        items(landmarks) {
+            LandmarkRow(landmark = it)
+        }
+    }
+}
+
+@Composable
+fun LandmarkRow(landmark: Landmark) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .background(color = MaterialTheme.colorScheme.primaryContainer)) {
+        Text(text = landmark.name,
+            //color = MaterialTheme.colorScheme.surfaceContainerLow,
+            style = MaterialTheme.typography.displayMedium,
+            modifier = Modifier.padding(2.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = landmark.country,
+            //color = MaterialTheme.colorScheme.surfaceContainerHigh,
+            style = MaterialTheme.typography.displaySmall,
+            modifier = Modifier.padding(2.dp))
+    }
 }
 
 
